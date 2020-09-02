f713907603cd4caf4e27717c50bd693f008b9fb6
Joe Bay <jbay55278@gmail.com>
2020-09-02 15:13:28 -0800

    built out most of my endpoints and got the logger and validate user id MW working

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const express = require('express');
-
+const db = require('./users/userDb')
+const userRouter = require('./users/userRouter')
 const server = express();
+
+server.use(express.json())
+server.use(logger())
+server.use('/api/users', userRouter)
+
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
+function logger(){
+  return (req, res, next)=> {
+      console.log(`the request was a ${req.method} was made to ${req.url} at ${new Date()}`)
+      next()
+  }
+}
 
-function logger(req, res, next) {}
+// function validateUserId(id){
+//   return (req, res, next) => {
+//     db.getById(id)
+//     .then(post => {
+//       if(post.id){
+//         req.user = post.id
+//       }
+//       else {
+//         res.status(400).json({message: 'there was an error'})
+//       }
+//     })
+//   }
+// }
 
 module.exports = server;
